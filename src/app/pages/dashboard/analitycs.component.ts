6c777b69ce61d7b6c44959d388e435c3e5856d9a
ESTUDIANTE <estudiante@ul.edu.co>
2019-09-05 20:24:24 -0500

    crear modulos y componentes

---
+++ src/app/pages/dashboard/analitycs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analitycs',
+  templateUrl: './analitycs.component.html',
+  styleUrls: ['./analitycs.component.css']
+})
+export class AnalitycsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
