89fa3b3277ad7b3568abbe1280252e5efa6f9617
anapolo <anapolo0913@gmail.com>
2019-09-12 15:12:33 -0500

    crear modulos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages/pages.component';
+import { AdministrationComponent } from './pages/administration/administration.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ModuleComponent } from './pages/settings/module/module.component';
+import { Administration1Component } from './pages/administration1/administration1.component';
+import { UsersComponent } from './pages/administration/users/users.component';
+import { RolesComponent } from './pages/administration/roles/roles.component';
+import { SettingComponent } from './pages/setting/setting.component';
+import { CategoriesComponent } from './pages/setting/categories/categories.component';
+import { AuthorsComponent } from './pages/setting/authors/authors.component';
+import { AreasComponent } from './pages/setting/areas/areas.component';
+import { SectionsComponent } from './pages/setting/sections/sections.component';
+import { BooksComponent } from './pages/setting/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent,LoginComponent
+    AppComponent,LoginComponent, DashboardComponent, PagesComponent, AdministrationComponent, SettingsComponent, ModuleComponent, Administration1Component, UsersComponent, RolesComponent, SettingComponent, CategoriesComponent, AuthorsComponent, AreasComponent, SectionsComponent, BooksComponent
   ],
   imports: [
     BrowserModule,AppRoutingModule
--- src/app/pages/setting/setting.component.spec.ts
+++ src/app/pages/setting/setting.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AnalitycsComponent } from './analitycs.component';
+import { SettingComponent } from './setting.component';
 
-describe('AnalitycsComponent', () => {
-  let component: AnalitycsComponent;
-  let fixture: ComponentFixture<AnalitycsComponent>;
+describe('SettingComponent', () => {
+  let component: SettingComponent;
+  let fixture: ComponentFixture<SettingComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AnalitycsComponent ]
+      declarations: [ SettingComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AnalitycsComponent);
+    fixture = TestBed.createComponent(SettingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
